7ec2f9b3b6b25dc6d4d84045e282b5417c934e37
Dominic K Savio <dominicksavio>
2021-06-05 21:18:36 +0530

    Port configured

--- main.go
+++ main.go
@@ -45,7 +45,13 @@ func main() {
 	router.POST("/save_form", FormHandler)
 	router.POST("/get_image_ip", ImageHandler) 
 	router.GET("/get_image", GetImageHandler) 
-	router.Run(":8080")
+	port, err := os.Getenv("PORT")
+    if err != nil {
+        port = "3000"
+    } 
+
+    router.Run(":"+port)
+    // router.Run(":8080")
 }
 func GetImageHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "image.html", gin.H{})
